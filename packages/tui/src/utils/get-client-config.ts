d17b1aede64c44d58a7939f39fa96f1a76f2f903
Madison <madison.bullard@gmail.com>
2025-04-09 16:38:26 -0400

    Fix "cannot read properties of undefined" error in TUI

--- packages/tui/src/utils/get-client-config.ts
+++ packages/tui/src/utils/get-client-config.ts
@@ -9,11 +9,25 @@ export function getClientConfig(client: Client, mcpServerName: string) {
 		fs.existsSync(path.dirname(configFilePath)) &&
 		fs.existsSync(configFilePath);
 
-	const existingConfig: McpConfig<string> | null = configExists
-		? JSON.parse(fs.readFileSync(configFilePath, "utf8"))
-		: null;
+	const existingConfig: Record<never, never> | McpConfig<string> | null =
+		configExists ? JSON.parse(fs.readFileSync(configFilePath, "utf8")) : null;
 
-	const specificConfig = existingConfig?.mcpServers[mcpServerName];
+	if (existingConfig && "mcpServers" in existingConfig) {
+		const specificConfig = existingConfig.mcpServers[mcpServerName];
+		return specificConfig;
+	}
 
-	return specificConfig;
+	return null;
 }
+
+// ERROR  Cannot read properties of undefined (reading ‘slack’)
+//  .npm/_npx/4ec18a4fef908e1b/node_modules/@madisonbullard/mcp-server-tui/dist/cli.js:60:52
+//  57:   const { configFilePath } = clientConfigs[client];
+//  58:   const configExists = fs.existsSync(path2.dirname(configFilePath)) &&
+//     fs.existsSync(configFilePath);
+//  59   const existingConfig = configExists ? JSON.parse(fs.readFileSync(configFilePath, “utf8”)) :
+//  : null;
+//  60:   const specificConfig = existingConfig?.mcpServers[mcpServerName];
+//  61:   return specificConfig;
+//  62: }
+//  63:
