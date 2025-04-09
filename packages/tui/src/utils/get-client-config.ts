b5bee6d3523b410d5d0ae3edc50297fc5004da3a
Madison <madison.bullard@gmail.com>
2025-04-09 16:38:38 -0400

    rm comment

--- packages/tui/src/utils/get-client-config.ts
+++ packages/tui/src/utils/get-client-config.ts
@@ -19,15 +19,3 @@ export function getClientConfig(client: Client, mcpServerName: string) {
 
 	return null;
 }
-
-// ERROR  Cannot read properties of undefined (reading ‘slack’)
-//  .npm/_npx/4ec18a4fef908e1b/node_modules/@madisonbullard/mcp-server-tui/dist/cli.js:60:52
-//  57:   const { configFilePath } = clientConfigs[client];
-//  58:   const configExists = fs.existsSync(path2.dirname(configFilePath)) &&
-//     fs.existsSync(configFilePath);
-//  59   const existingConfig = configExists ? JSON.parse(fs.readFileSync(configFilePath, “utf8”)) :
-//  : null;
-//  60:   const specificConfig = existingConfig?.mcpServers[mcpServerName];
-//  61:   return specificConfig;
-//  62: }
-//  63:
